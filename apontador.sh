33057f0bf59ce98dfcf492a1bbec0178c0d92660
Henrique Soares da Silva <kabessao.rock@gmail.com>
2020-07-16 17:28:55 -0300

    Update apontador.sh

--- apontador.sh
+++ apontador.sh
@@ -98,13 +98,13 @@ then
 ------------------------------" >> ~/ApontadorLog.txt
 	($Comando | while read line 
 	do
-	    if echo $line | grep "Gravado:" ; then
+	    if echo $line | grep "Gravado" ; then
 	    	OUTPUT="Status da linha $((++var)): $line"
 	    else 
 	    	OUTPUT="$line"
 	    fi
 	    echo "${OUTPUT}" >> ~/ApontadorLog.txt
-	    echo "#${OUTPUT}
+	    echo "#${OUTPUT}"
 	done)| yad --enable-log="processando" --progress --title=Gravando --pulsate --auto-kill --log-expanded
  else 
 	echo "Ação cancelada pelo usuario"
